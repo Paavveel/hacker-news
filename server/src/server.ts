2150e29496f28b73722178505a10491b510c52c6
Paavveel <pa.tomilin@gmail.com>
2022-11-10 14:01:07 +0300

    feat: add bodyParser

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 
 const PORT = 3500;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
