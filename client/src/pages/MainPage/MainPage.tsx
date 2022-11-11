631cb770701dcd1917626dc0f055e5e4c7b0bfa1
Paavveel <pa.tomilin@gmail.com>
2022-11-11 18:53:11 +0300

    feat: add header MainPage

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,24 @@
+import cn from 'classnames';
+import { SlRefresh } from 'react-icons/sl';
+import { Button, Typography } from '../../components';
+import styles from './MainPage.module.css';
+
 export const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <>
+      <div className={cn(styles.container)}>
+        <Typography
+          color='secondary'
+          size='l'
+          weight='bold'
+          className={styles.label}
+        >
+          Latest 100 news
+        </Typography>
+        <Button appearance='primary' className={cn(styles.button)}>
+          <SlRefresh className={cn(styles.icon)} />
+        </Button>
+      </div>
+    </>
+  );
 };
