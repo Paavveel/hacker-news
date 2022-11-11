3cd5e05de7486872c1f158fc51394fbc385e1ed1
Paavveel <pa.tomilin@gmail.com>
2022-11-11 14:22:49 +0300

    feat: add Footer styles

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,3 +1,20 @@
-export const Footer = () => {
-  return <div>Footer</div>;
+import cn from 'classnames';
+import { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
+import styles from './Footer.module.css';
+
+type IFooterProps = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
+
+export const Footer: FC<IFooterProps> = () => {
+  return (
+    <footer className={cn(styles.footer)}>
+      by
+      <a
+        href='https://github.com/Paavveel'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Paavveel
+      </a>
+    </footer>
+  );
 };
