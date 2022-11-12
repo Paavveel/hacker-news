644e159093b79b96fa375118818a14f47fc5595f
Pavel T <pa.tomilin@gmail.com>
2022-11-12 16:56:47 +0300

    fix: Main page header

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,22 +1,13 @@
 import cn from 'classnames';
-import { SlRefresh } from 'react-icons/sl';
-import { Button, Typography } from '../../components';
+import { Button } from '../../components';
 import styles from './MainPage.module.css';
 
 export const MainPage = () => {
   return (
     <>
       <div className={cn(styles.container)}>
-        <Typography
-          color='secondary'
-          size='l'
-          weight='bold'
-          className={styles.label}
-        >
-          Latest 100 news
-        </Typography>
-        <Button appearance='primary' className={cn(styles.button)}>
-          <SlRefresh className={cn(styles.icon)} />
+        <Button appearance='primary'>
+          <span>Update</span>
         </Button>
       </div>
     </>
