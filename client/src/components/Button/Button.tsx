40e8cd26b599a8862ec5108fd7dfaf92f8183632
Pavel T <pa.tomilin@gmail.com>
2022-11-12 16:56:27 +0300

    feat: add Button loading state

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import cn from 'classnames';
 import { ButtonHTMLAttributes, DetailedHTMLProps, FC, ReactNode } from 'react';
-import styles from './button.module.css';
+import styles from './Button.module.css';
 
 interface IButtonProps
   extends DetailedHTMLProps<
@@ -8,22 +8,27 @@ interface IButtonProps
     HTMLButtonElement
   > {
   appearance: 'primary' | 'ghost';
+  loading?: boolean;
   children?: ReactNode;
 }
 
 export const Button: FC<IButtonProps> = ({
   children,
   appearance,
+  loading = false,
   className,
   ...props
 }) => (
   <button
+    disabled={loading}
     className={cn(styles.button, className, {
       [styles.primary]: appearance === 'primary',
       [styles.ghost]: appearance === 'ghost',
+      [styles.disabled]: loading,
     })}
     {...props}
   >
+    {loading && <div className={styles.loading} />}
     {children}
   </button>
 );
