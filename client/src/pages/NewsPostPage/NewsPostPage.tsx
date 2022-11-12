041095337464390ee0d3bb6cf26932110d35744a
Pavel T <pa.tomilin@gmail.com>
2022-11-12 20:12:49 +0300

    feat: add header in the NewsPostPage component

--- client/src/pages/NewsPostPage/NewsPostPage.tsx
+++ client/src/pages/NewsPostPage/NewsPostPage.tsx
@@ -1,3 +1,33 @@
-export const NewsPostPage = () => {
-  return <div>NewsPostPage</div>;
+import cn from 'classnames';
+import { FC } from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { useHistory } from 'react-router-dom';
+import { Button, Typography } from '../../components';
+
+import styles from './NewsPostPage.module.css';
+
+export const NewsPostPage: FC = () => {
+  const history = useHistory();
+
+  return (
+    <>
+      <div className={cn(styles.container)}>
+        <Button appearance='ghost' onClick={() => history.goBack()}>
+          <IoIosArrowBack />
+          <Typography
+            color='special'
+            size='l'
+            weight='bold'
+            className={styles.label}
+          >
+            Back to the news
+          </Typography>
+        </Button>
+
+        <Button appearance='primary'>
+          <span>Update</span>
+        </Button>
+      </div>
+    </>
+  );
 };
