c89a1a13fb10b2c1c821e0254774abb5ce0280b6
Pavel T <pa.tomilin@gmail.com>
2022-11-13 20:12:20 +0300

    fix: comments width

--- client/src/pages/NewsPostPage/NewsPostPage.tsx
+++ client/src/pages/NewsPostPage/NewsPostPage.tsx
@@ -1,13 +1,35 @@
 import cn from 'classnames';
 import { FC } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
-import { useHistory } from 'react-router-dom';
-import { Button, NewsPost, Typography } from '../../components';
+import { useHistory, useParams } from 'react-router-dom';
+import {
+  Button,
+  Comment,
+  NewsPost,
+  Spinner,
+  Title,
+  Typography,
+} from '../../components';
+import { useGetPostWithCommentsQuery } from '../../services/newsApi';
+import { IQuery } from '../../types';
 
 import styles from './NewsPostPage.module.css';
 
 export const NewsPostPage: FC = () => {
   const history = useHistory();
+  const { id } = useParams<IQuery>();
+
+  const { data, isLoading, isFetching, isError, refetch } =
+    useGetPostWithCommentsQuery(parseInt(id));
+
+  if (!data) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <Title>Error! Something went wrong 😔</Title>;
+  }
+
+  const { kids = [], descendants = 0 } = data;
 
   return (
     <>
@@ -24,16 +46,23 @@ export const NewsPostPage: FC = () => {
           </Typography>
         </Button>
 
-        <Button appearance='primary'>
+        <Button loading={isFetching} appearance='primary' onClick={refetch}>
           <span>Update</span>
         </Button>
       </div>
-      <NewsPost newsId={1} />
-      <div className={styles.comments}>
-        <Typography size='l' weight='bold' color='secondary'>
-          {10} Comments
-        </Typography>
-      </div>
+      <NewsPost {...data} />
+      {isLoading || isFetching ? (
+        <Spinner />
+      ) : (
+        <div className={styles.comments}>
+          <Typography size='l' weight='bold' color='secondary'>
+            {descendants} Comments
+          </Typography>
+          {kids.map((item: any) => (
+            <Comment key={item.id} {...item} root />
+          ))}
+        </div>
+      )}
     </>
   );
 };
