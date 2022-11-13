22aa2c7604f168b6a3447b29d319d04cc7619474
Pavel T <pa.tomilin@gmail.com>
2022-11-13 21:23:55 +0300

    feat: add NotFound page

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,24 @@
-export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+import cn from 'classnames';
+import { FC } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button, Main, Typography } from '../../components';
+import styles from './NotFoundPage.module.css';
+
+export const NotFoundPage: FC = () => {
+  const history = useHistory();
+
+  return (
+    <Main>
+      <div className={cn(styles.container)}>
+        <Button appearance='primary' onClick={() => history.goBack()}>
+          Back to the news
+        </Button>
+      </div>
+      <div className={styles.message}>
+        <Typography size='l' color='special' weight='black'>
+          404 Not Found
+        </Typography>
+      </div>
+    </Main>
+  );
 };
